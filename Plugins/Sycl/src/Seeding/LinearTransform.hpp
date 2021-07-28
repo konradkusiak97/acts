f45275ac91c49e7884c4113a493173d5c0855885
Konrad Kusiak <kkusiak@atspot01.cern.ch>
2021-07-28 17:37:59 +0200

    Made use of the VecMem types.

--- Plugins/Sycl/src/Seeding/LinearTransform.hpp
+++ Plugins/Sycl/src/Seeding/LinearTransform.hpp
@@ -17,6 +17,9 @@
 // SYCL include(s).
 #include <CL/sycl.hpp>
 
+// VecMem includes
+#include "vecmem/containers/device_vector.hpp"
+
 // System include(s).
 #include <cassert>
 #include <cstdint>
@@ -35,21 +38,19 @@ class LinearTransform {
 
  public:
   /// Constructor with all the necessary arguments
-  LinearTransform(uint32_t nMiddleSPs,
-                  const device_array<DeviceSpacePoint>& middleSPs,
-                  uint32_t nOtherSPs,
-                  const device_array<DeviceSpacePoint>& otherSPs,
-                  const device_array<uint32_t>& middleIndexLUT,
-                  const device_array<uint32_t>& otherIndexLUT, uint32_t nEdges,
-                  device_array<DeviceLinEqCircle>& resultArray)
-      : m_nMiddleSPs(nMiddleSPs),
-        m_middleSPs(middleSPs.get()),
-        m_nOtherSPs(nOtherSPs),
-        m_otherSPs(otherSPs.get()),
-        m_middleIndexLUT(middleIndexLUT.get()),
-        m_otherIndexLUT(otherIndexLUT.get()),
+  LinearTransform(const vecmem::data::vector_view<const detail::DeviceSpacePoint>& middleSPs,
+                  const vecmem::data::vector_view<const detail::DeviceSpacePoint>& otherSPs,
+                  const vecmem::data::vector_view<u_int32_t>& middleIndexLUT,
+                  const vecmem::data::vector_view<u_int32_t>& otherIndexLUT, uint32_t nEdges,
+                  vecmem::data::vector_view<DeviceLinEqCircle>& resultArray)
+      : m_nMiddleSPs(middleSPs.size()),
+        m_middleSPs(middleSPs),
+        m_nOtherSPs(otherSPs.size()),
+        m_otherSPs(otherSPs),
+        m_middleIndexLUT(middleIndexLUT),
+        m_otherIndexLUT(otherIndexLUT),
         m_nEdges(nEdges),
-        m_resultArray(resultArray.get()) {}
+        m_resultArray(resultArray) {}
 
   /// Operator performing the coordinate linear transformation
   void operator()(cl::sycl::nd_item<1> item) const {
@@ -59,22 +60,29 @@ class LinearTransform {
       return;
     }
 
+    // Create the device VecMem vectors from the views.
+    vecmem::device_vector<const detail::DeviceSpacePoint> device_middleSPs(m_middleSPs);
+    vecmem::device_vector<const detail::DeviceSpacePoint> device_otherSPs(m_otherSPs);
+    vecmem::device_vector<uint32_t> device_middleIndexLUT(m_middleIndexLUT);
+    vecmem::device_vector<uint32_t> device_otherIndexLUT(m_otherIndexLUT);
+    vecmem::device_vector<DeviceLinEqCircle> device_resultArray(m_resultArray);
+
     // Translate this one index into indices in the spacepoint arrays.
     // Note that using asserts with the CUDA backend of dpc++ is not working
     // quite correctly at the moment. :-( So these checks may need to be
     // disabled if you need to build for an NVidia backend in Debug mode.
-    const uint32_t middleIndex = m_middleIndexLUT[idx];
+    const uint32_t middleIndex = device_middleIndexLUT[idx];
     assert(middleIndex < m_nMiddleSPs);
     (void)m_nMiddleSPs;
-    const uint32_t otherIndex = m_otherIndexLUT[idx];
+    const uint32_t otherIndex = device_otherIndexLUT[idx];
     assert(otherIndex < m_nOtherSPs);
     (void)m_nOtherSPs;
 
     // Create a copy of the spacepoint objects for the current thread. On
     // dedicated GPUs this provides a better performance than accessing
     // variables one-by-one from global device memory.
-    const DeviceSpacePoint middleSP = m_middleSPs[middleIndex];
-    const DeviceSpacePoint otherSP = m_otherSPs[otherIndex];
+    const DeviceSpacePoint middleSP = device_middleSPs[middleIndex];
+    const DeviceSpacePoint otherSP = device_otherSPs[otherIndex];
 
     // Calculate some "helper variables" for the coordinate linear
     // transformation.
@@ -105,30 +113,30 @@ class LinearTransform {
         iDeltaR2;
 
     // Store the result object in device global memory.
-    m_resultArray[idx] = result;
+    device_resultArray.push_back(result);
     return;
   }
 
  private:
   /// Total number of middle spacepoints
   uint32_t m_nMiddleSPs;
-  /// Pointer to the middle spacepoints (in global device memory)
-  const DeviceSpacePoint* m_middleSPs;
+  /// VecMem vector view to the middle spacepoints (in global device memory)
+  const vecmem::data::vector_view<const detail::DeviceSpacePoint> m_middleSPs;
   /// Total number of "other" (bottom or top) spacepoints
   uint32_t m_nOtherSPs;
-  /// Pointer to the "other" (bottom or top) spacepoints (in global device mem.)
-  const DeviceSpacePoint* m_otherSPs;
+  /// VecMem vector view to the "other" (bottom or top) spacepoints (in global device mem.)
+  const vecmem::data::vector_view<const detail::DeviceSpacePoint> m_otherSPs;
 
   /// Look-Up Table from the iteration index to the middle spacepoint index
-  const uint32_t* m_middleIndexLUT;
+  const vecmem::data::vector_view<u_int32_t> m_middleIndexLUT;
   /// Loop-Up Table from the iteration index to the "other" spacepoint index
-  const uint32_t* m_otherIndexLUT;
+  const vecmem::data::vector_view<u_int32_t> m_otherIndexLUT;
 
   /// Total number of elements in the result array
   uint32_t m_nEdges;
 
   /// The result array in device global memory
-  DeviceLinEqCircle* m_resultArray;
+  vecmem::data::vector_view<DeviceLinEqCircle> m_resultArray;
 
 };  // class LinearTransform
 
